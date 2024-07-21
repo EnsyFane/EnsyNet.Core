998c2a93ed130faa85ed7827c0943f71435ceb01
Stefan Tataran <fane_ecl@yahoo.com>
2024-07-21 12:10:51 +0300

    Fix test script

--- infra/scripts/run-tests.ps1
+++ infra/scripts/run-tests.ps1
@@ -17,18 +17,18 @@ ThrowOnError "Failed to start the database container"
 
 New-Item -ItemType Directory -Force -Path ./test-results
 
-dotnet add ./src/Tests/EnsyNet.DataAccess.EntityFramework.Tests package JetBrains.dotCover.CommandLineTools.linux-x64 --version 2023.3.3 --package-directory ./nuget
+dotnet add ./src/Core/Tests/EnsyNet.DataAccess.EntityFramework.Tests package JetBrains.dotCover.CommandLineTools.linux-x64 --version 2023.3.3 --package-directory ./nuget
 
 dotnet sonarscanner begin -o:"stefan-tataran" -k:"EnsyFane_EnsyNet.Core" -d:sonar.host.url="https://sonarcloud.io" -d:sonar.token="$env:SONAR_TOKEN" -d:sonar.cs.dotcover.reportsPaths="./code-coverage/integration-tests/coverage.html" -d:sonar.coverage.exclusions="**/Tests/**,**/Sample/**" -d:sonar.exclusions="**/.vs/**,**/*.sln"
 ThrowOnError "Failed to start Sonar Scanner session"
 
-dotnet build ./src/Tests/EnsyNet.DataAccess.EntityFramework.Tests/EnsyNet.DataAccess.EntityFramework.Tests.csproj
+dotnet build ./src/Core/Tests/EnsyNet.DataAccess.EntityFramework.Tests/EnsyNet.DataAccess.EntityFramework.Tests.csproj
 ThrowOnError "Failed to build"
 
-./nuget/jetbrains.dotcover.commandlinetools.linux-x64/2023.3.3/tools/dotCover.sh dotnet --Filters="-:module=Humanizer;-:module=vstest.console" --Output="./test-results/coverage.dcvr" -- test ./src/EnsyNet.sln --no-restore --no-build
+./nuget/jetbrains.dotcover.commandlinetools.linux-x64/2023.3.3/tools/dotCover.sh dotnet --Filters="-:module=Humanizer;-:module=vstest.console" --Output="./test-results/coverage.dcvr" -- test ./src/Core/EnsyNet.sln --no-restore --no-build
 ThrowOnError "Failed to run tests"
 
-./nuget/jetbrains.dotcover.commandlinetools.linux-x64/2023.3.3/tools/dotCover.sh report --Source="./test-results/coverage.dcvr" --Output="./code-coverage/integration-tests/coverage.html" --ReportType="HTML" --SourcesSearchPaths="./src/EnsyNet." --ExcludeFileMasks="./src/Tests/**"
+./nuget/jetbrains.dotcover.commandlinetools.linux-x64/2023.3.3/tools/dotCover.sh report --Source="./test-results/coverage.dcvr" --Output="./code-coverage/integration-tests/coverage.html" --ReportType="HTML" --SourcesSearchPaths="./src/Core/EnsyNet." --ExcludeFileMasks="./src/Core/Tests/**"
 ThrowOnError "Failed to generate code coverage report"
 
 dotnet sonarscanner end -d:sonar.token="$env:SONAR_TOKEN"
