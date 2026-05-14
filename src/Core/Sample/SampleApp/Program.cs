d9e8db76d5db1a8ece77902062769d95723b09fb
Stefan Tataran <fane_ecl@yahoo.com>
2026-05-14 13:29:25 +0300

    [Chore] Update sample (#54)

--- src/Core/Sample/SampleApp/Program.cs
+++ src/Core/Sample/SampleApp/Program.cs
@@ -3,8 +3,6 @@ using EnsyNet.DataAccess.Abstractions.Errors;
 using EnsyNet.DataAccess.Abstractions.Interfaces;
 using EnsyNet.DataAccess.Abstractions.Models;
 
-using System.Linq.Expressions;
-
 _ = Result.Ok();
 Result<string> r2 = Result.Ok("Hello, World!");
 
@@ -36,7 +34,7 @@ await repository.GetManyByExpression(e => e.UpdatedAt != null, pagination, sorti
 await repository.Insert(entity, cts.Token);
 await repository.Insert(entities, cts.Token);
 await repository.Update(entity.Id, x => x.AddUpdate(e => e.Name, _ => "NewName"), cts.Token);
-await repository.Update(new Dictionary<Guid, Expression<Func<EntityUpdates<TestEntity>, EntityUpdates<TestEntity>>>>{ { entity.Id, x => x.AddUpdate(e => e.Name, _ => "NewName") } }, cts.Token);
+await repository.Update(new Dictionary<Guid, Action<EntityUpdates<TestEntity>>>{ { entity.Id, x => x.AddUpdate(e => e.Name, _ => "NewName") } }, cts.Token);
 await repository.SoftDelete(Guid.NewGuid(), cts.Token);
 await repository.SoftDelete(new List<Guid> { Guid.NewGuid() }, cts.Token);
 await repository.SoftDelete(e => e.UpdatedAt != null, cts.Token);
@@ -46,5 +44,5 @@ await repository.HardDelete(e => e.UpdatedAt != null, cts.Token);
 
 internal sealed record TestEntity : DbEntity
 {
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
 }
